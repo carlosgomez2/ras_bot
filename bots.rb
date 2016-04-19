2f5c90562390d28f8c94b970a5e784aa3b00bc51
Carlos Gomez <carlosgomez.deb@gmail.com>
2016-04-19 11:42:20 -0500

    added keys

--- bots.rb
+++ bots.rb
@@ -1,21 +1,31 @@
 require 'twitter_ebooks'
+require 'dotenv'
+
+# Load enviroment variables file
+Dotenv.load(".env")
+
+# Model for tweets
+MODEL_PATH = "model/Tweets.model"
 
 # This is an example bot definition with event handlers commented out
 # You can define and instantiate as many bots as you like
 
-class MyBot < Ebooks::Bot
-  # Configuration here applies to all MyBots
+class Ras_bot < Ebooks::Bot
+
+  attr_accessor :model
+
+  # Configuration here applies to all Bots
   def configure
     # Consumer details come from registering an app at https://dev.twitter.com/
     # Once you have consumer details, use "ebooks auth" for new access tokens
-    self.consumer_key = ENV["consumer_key"] # Your app consumer key
-    self.consumer_secret = ENV["consumer_secret"] # Your app consumer secret
+    self.consumer_key = ENV["CONSUMER_KEY"] # Your app consumer key
+    self.consumer_secret = ENV["CONSUMER_SECRET"] # Your app consumer secret
 
     # Users to block instead of interacting with
     self.blacklist = []
 
     # Range in seconds to randomize delay when bot.delay is called
-    self.delay_range = 1..6
+    self.delay_range = 3..8
   end
 
   # Random time function
@@ -34,7 +44,8 @@ class MyBot < Ebooks::Bot
       # See https://github.com/jmettraux/rufus-scheduler
       # tweet("hi")
       # pictweet("hi", "cuteselfie.jpg")
-      model = Ebooks::Model.load("model/AristeguiOnline.model")
+      log "Loading model #{MODEL_PATH}"
+      model = Ebooks::Model.load("MODEL_PATH")
       text = model.make_statement(140)
       tweet(text)
     end
@@ -42,7 +53,7 @@ class MyBot < Ebooks::Bot
 
   def on_message(dm)
     # Reply to a DM
-    model = Ebooks::Model.load("model/AristeguiOnline.model")
+    model = Ebooks::Model.load("MODEL_PATH")
     reply(dm, model.make_response(tweet.text, 130))
     # reply(dm, "Hola! Gracias por seguir mi canal")
   end
@@ -54,7 +65,7 @@ class MyBot < Ebooks::Bot
 
   def on_mention(tweet)
     # Reply to a mention
-    model = Ebooks::Model.load("model/AristeguiOnline.model")
+    model = Ebooks::Model.load("MODEL_PATH")
     text = model.make_response(tweet.text, 130)
     reply(tweet, meta(tweet).reply_prefix + text)
     # reply(tweet, "Hola, como estas?")
@@ -62,7 +73,7 @@ class MyBot < Ebooks::Bot
 
   def on_timeline(tweet)
     # Reply to a tweet in the bot's timeline
-    model = Ebooks::Model.load("model/AristeguiOnline.model")
+    model = Ebooks::Model.load("MODEL_PATH")
     unless tweet.text.include?("@") # we don't want to take part at other conversations
       text = model.make_response(tweet.text, 130)
       # reply(tweet, meta(tweet).reply_prefix + text)
@@ -83,12 +94,12 @@ class MyBot < Ebooks::Bot
 
   def load_model
     # Load model to Markov chain
-    model = Ebooks::Model.load("model/AristeguiOnline.model")
+    model = Ebooks::Model.load("MODEL_PATH")
   end
 end
 
-# Make a MyBot and attach it to an account
-MyBot.new("RasAlGhul_bot") do |bot|
-  bot.access_token = ENV["access_token"] # Token connecting the app to this account
-  bot.access_token_secret = ENV["access_token_secret"] # Secret connecting the app to this account
+# Make a Ras_bot and attach it to an account
+Ras_bot.new("RasAlGhul_bot") do |bot|
+  bot.access_token = ENV["ACCESS_TOKEN"] # Token connecting the app to this account
+  bot.access_token_secret = ENV["ACCESS_TOKEN_SECRET"] # Secret connecting the app to this account
 end
